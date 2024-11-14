7b1c257ac4ffc6aa812450b7847da6d8fe501f57
Khairiddin Sakhiev <93430063+KhairiddinSakhiev@users.noreply.github.com>
2024-11-14 19:03:06 +0500

    init models

---
+++ apis/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    pass
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts')
+    created_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title
+    
